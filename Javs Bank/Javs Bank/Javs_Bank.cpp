c71462fd292ac988157d0b43594d57556626ee36
Javier Cardoso <Cardoso.Javier@outlook.com>
2022-03-11 02:05:33 -0600

    Creation of project.
    The project was created.
    
    - A description of the whole project was given.
    
    - main function was created and prints
      "hello world" to test it.

---
+++ Javs Bank/Javs Bank/Javs_Bank.cpp
@@ -0,0 +1,30 @@
+/*
+*	Developer: Javier Cardoso.
+*	Project Name: Javs Bank
+*
+*	Description:
+*		A bank system and interface where the user will be able to
+*		make basic bank transactions:
+*
+*			Fist stage:
+*				- sign in or login.
+*				- deposit.
+*				- withdraw.
+*				- consult balance.
+*/
+
+
+// libraries used
+
+#include <iostream>
+
+using namespace std;
+
+
+// main function
+int main(void) {
+
+	cout << "Hello world" << endl;
+
+	return 0;
+}
